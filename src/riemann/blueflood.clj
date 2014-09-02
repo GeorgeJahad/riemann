7ffa025a52ad69d873fe828311d895f5e59c4474
George Jahad <github@blackbirdsystems.net>
2014-09-02 15:31:49 -0700

    Initial commit of Dominic Lobue's Blueflood plugin

---
+++ src/riemann/blueflood.clj
@@ -0,0 +1,35 @@
+(require '[cheshire.core :as json]
+         '[clojure.string :as s])
+(use 'aleph.http)
+
+
+(defn prep-event-for-bf [ev]
+  {:collectionTime (:time ev)
+   :ttlInSeconds (if-let [rttl (:ttl ev)]
+                   rttl 2592000)
+   :metricValue (:metric ev)
+   :metricName (s/join "." [(:host ev) (:service ev)])})
+
+(defn bf-body [evs]
+  (->> evs
+       (map prep-event-for-bf)
+       json/generate-string))
+
+(defn log-bf-body [evs]
+  (let [r (bf-body evs)]
+    (info "bf-body" r)
+    r))
+
+
+
+(defn bf-ingest [evs]
+  ; TODO: future performance enhancements: switch to async client, use keep http connection open
+  (sync-http-request
+    {:method :post
+     :url "http://192.168.5.3:19000/v1.0/tenant-id/experimental/metrics"
+     :content-type :json
+     :body (bf-body evs)}))
+
+
+(defn log-bf-ingest [evs]
+  (info "ingest_output" (bf-ingest evs)))
