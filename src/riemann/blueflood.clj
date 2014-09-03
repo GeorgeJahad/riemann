e9805214d4b3a66d22e9c13b42555f3e4f6c50df
George Jahad <github@blackbirdsystems.net>
2014-09-03 10:34:00 -0700

    fixed up url/added defaults

--- src/riemann/blueflood.clj
+++ src/riemann/blueflood.clj
@@ -6,10 +6,16 @@
             [clojure.tools.logging :as logging]))
 
 (def version "0.1")
+(def url-template "http://%s:%s/v2.0/%s/ingest")
+(def defaults
+  {:ttl 2592000
+   :host "localhost"
+   :port "19000"
+   :tenant-id "tenant-id"})
+
 (defn prep-event-for-bf [ev]
   {:collectionTime (:time ev)
-   :ttlInSeconds (if-let [rttl (:ttl ev)]
-                   rttl 2592000)
+   :ttlInSeconds (or (:ttl ev) (defaults :ttl))
    :metricValue (:metric ev)
    :metricName (s/join "." [(:host ev) (:service ev)])})
 
@@ -25,14 +31,27 @@
 
 
 
-(defn bf-ingest [evs]
+(defn blueflood-ingest [{:keys [host port tenant-id]
+                         :as opts
+                         :or {host (defaults :host)
+                              port (defaults :port)
+                              tenant-id (defaults :tenant-id)}}]
   ; TODO: future performance enhancements: switch to async client, use keep http connection open
-  (logging/info version
-    {:method :post
-     :url "http://192.168.5.3:19000/v1.0/tenant-id/experimental/metrics"
-     :content-type :json
-     :body (bf-body evs)}))
+  (let [url (format url-template host port tenant-id)]
+    (fn [evs]
+#_      (client/post url
+               {:body (bf-body evs)
+                :socket-timeout 5000
+                :conn-timeout 5000
+                :content-type :json
+                :accept :json
+                :throw-entire-message? true})
+      (logging/info version
+                    {:method :post
+                     :url url
+                     :content-type :json
+                     :body (bf-body evs)}))))
 
 
-(defn log-bf-ingest [evs]
+#_(defn log-bf-ingest [evs]
   (logging/info "ingest_output" (bf-ingest evs)))
